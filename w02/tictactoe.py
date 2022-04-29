51be2dd0ab4fcabe5aebad66230af7714d9e4936
Diego Feresin <diegoferesin@gmail.com>
2022-04-28 23:44:20 -0300

    first commit

---
+++ w02/tictactoe.py
@@ -0,0 +1,26 @@
+# Requirements
+# 1. The program must have a comment with assignment and author names.
+# 2. The program must have at least one if/then block.
+# 3. The program must have at least one while loop.
+# 4. The program must have more than one function.
+# 5. The program must have a function called main.
+
+'''
+Tic-Tac-Toe Assignment
+Author: Diego Feresin
+'''
+
+def main():
+    condition_to_validate = None
+
+    if condition_to_validate:
+        print("")
+
+    while True:
+        print()
+
+def function_one():
+    return ""
+
+def function_two():
+    return ""
